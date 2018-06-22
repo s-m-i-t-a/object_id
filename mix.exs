94ff43726fe4830a8d3d54eb6500e3c6ed3650a1
Jindrich K. Smitka <jsmitka@smita.info>
2018-06-22 12:29:12 +0200

    Added dialyzer

--- mix.exs
+++ mix.exs
@@ -4,9 +4,11 @@ defmodule ObjectId.MixProject do
   def project do
     [
       app: :object_id,
+      description: "Mongo BSON ObjectID helper functions",
       version: "0.1.0",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
+      dialyzer: dialyzer_base() |> dialyzer_ptl(System.get_env("SEMAPHORE_CACHE_DIR")),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
@@ -37,7 +39,59 @@ defmodule ObjectId.MixProject do
       {:credo, "~> 0.9", only: [:dev, :test]},
       {:excoveralls, "~> 0.9", only: :test},
       {:mongodb, "~> 0.4.6"},
-      {:result, "~> 1.1"}
+      {:result, "~> 1.1"},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
     ]
+  end
+
+  defp package do
+    [
+      maintainers: [
+        "Jindrich K. Smitka <smitka.j@gmail.com>"
+      ],
+      licenses: ["BSD"],
+      links: %{
+        "GitHub" => "https://github.com/s-m-i-t-a/object_id"
+      }
+    ]
+  end
+
+  defp dialyzer_base() do
+    [plt_add_deps: :transitive]
+  end
+
+  defp dialyzer_ptl(base, nil) do
+    base
+  end
+
+  defp dialyzer_ptl(base, path) do
+    base ++
+      [
+        plt_core_path: path,
+        plt_file:
+          Path.join(
+            path,
+            "dialyxir_erlang-#{otp_vsn()}_elixir-#{System.version()}_deps-dev.plt"
+          )
+      ]
+  end
+
+  defp otp_vsn() do
+    major = :erlang.system_info(:otp_release) |> List.to_string()
+    vsn_file = Path.join([:code.root_dir(), "releases", major, "OTP_VERSION"])
+
+    try do
+      {:ok, contents} = File.read(vsn_file)
+      String.split(contents, "\n", trim: true)
+    else
+      [full] ->
+        full
+
+      _ ->
+        major
+    catch
+      :error, _ ->
+        major
+    end
   end
 end
